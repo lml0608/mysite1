d1723af20821bce93a96821b6d58e1d5f687c616
liubin <liumeile0608@163.com>
2018-05-29 16:55:17 +0800

    cd

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$',views.index),
+    url(r'^$',views.index,),
+    url(r'^register/$', views.register,name='reg'),
+    #有名分组
+    url(r'^article/(?P<year>\d{4})/(?P<month>\d{2})/$',views.article_year_month),
+    # 无名分组
+    url(r'^article/(\d{4})/(\d{2})/(\d{2})/$',views.article_year_month_day),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse,render_to_response,redirect
+from django.http import HttpResponse,HttpRequest
+from django.template import RequestContext
 
 import datetime
 
@@ -12,3 +13,38 @@ def index(request):
     content = {'time':datetime.datetime.now()}
 
     return render(request,'blog/index.html',content)
+
+def article_year_month(request,year, month):
+
+    #content = {'year':y, 'month':m}
+
+    return HttpResponse("year:%s month:%s"%(year,month))
+
+
+
+def article_year_month_day(request,year,month,day):
+    #content = {'year':year, 'month':month,'day':day}
+
+    return HttpResponse("year:%s month:%s day:%s"%(year,month,day))
+
+
+
+def register(request):
+
+
+    print(request.path) #/blog/register/
+
+    print(request.get_full_path())  #/blog/register/?uname=liubin&age=28&hobby=1&hobby=2
+
+
+    print(request.GET)
+
+
+    if request.method == 'POST':
+        print(request.POST) #{'csrfmiddlewaretoken': ['HM2NkUXe4ANk23Zt0UlhZTm07NEcgI3Z7kdudWHCwWRHbXwTsfhayxsaYBH2fiFs'],
+        #  'uname': ['343'], 'age': ['3545'], 'hobby': ['3']}>
+    return render_to_response('blog/register.html')
+
+
+
+
